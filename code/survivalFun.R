5a2e1425d78166a8f3575f48184523b4aeb040b0
aarizvi <aarizv@gmail.com>
2018-06-02 11:49:46 -0400

    implementation of regular survival function from survival package

---
+++ code/survivalFun.R
@@ -0,0 +1,22 @@
+# survivalFun.R
+library(survival)
+survivalFun <- function(SNP,
+                        pheno.file,
+                        time.to.event,
+                        event,
+                        covariates)
+{
+    formula <- paste0("Surv(time=",
+                      time.to.event,
+                      ", event=",
+                      event,
+                      ") ~ SNP + ",
+                      paste(covariates, collapse=" + "))
+    survFitMod <- function(SNP){
+        res <- coxph(as.formula(formula), data=data.frame(pheno.file))   
+        m <- summary(res)
+        m <- c(m$coef[1,], n=m$n)
+        return(m)
+    }
+    survFitMod(SNP)
+}
